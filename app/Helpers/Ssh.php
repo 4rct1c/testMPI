ff9094eb4bce516ca4493c81d9d1f82eafd9b883
vtsybykov@gmail.com <Arct1cW138>
2024-02-02 23:18:33 +0800

    SSH attempts

--- app/Helpers/Ssh.php
+++ app/Helpers/Ssh.php
@@ -8,6 +8,13 @@ class Ssh extends OriginalSsh
 {
 
 
+    public const USER = 'Arct1cW';
+
+    public const HOST = '91.197.1.40';
+
+    public const KEY_PATH = 'C:\Users\baika\.ssh\mpi-test';
+
+    public const PORT = 22;
     /**
      * @param string|array $command
      *
--- app/Http/Controllers/SSHController.php
+++ app/Http/Controllers/SSHController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Artisan;
 use JetBrains\PhpStorm\NoReturn;
 use App\Helpers\Ssh;
 
@@ -10,25 +9,42 @@ class SSHController extends Controller
 {
 
     public function test(){
+        self::secondAttempt();
         self::sendCommand();
+    }
+
+    private static function secondAttempt() : void
+    {
+        $resource = ssh2_connect('91.197.1.40', 22, 'client_to_server');
+        dd($resource);
+
     }
 
     #[NoReturn] public static function sendCommand() : void
     {
-        $user = 'Arct1cW';
-        $host = '91.197.1.40';
-//        $pkey = '/Users/baika/.ssh/mpi-test';
-        $pkey = 'C:\Users\baika\.ssh\mpi-test';
-        $port = 22;
-        $ssh = Ssh::create($user, $host, $port);
-        $ssh->usePrivateKey($pkey);
+        $command = 'ls';
+        $ssh = Ssh::create(Ssh::USER, Ssh::HOST, Ssh::PORT);
+        $ssh->usePrivateKey(Ssh::KEY_PATH);
         $ssh->removeBash();
-        dump($ssh->getExecuteCommand('ls -la'));
+        dump($ssh->getExecuteCommand($command));
         dump($ssh);
-        $process = $ssh->execute('ls -la');
+        $process = $ssh->execute($command);
+        dump($process->getCommandLine());
         dump($process->getOutput());
         dump($process->getErrorOutput());
         dump($process->isSuccessful());
         dd($process);
+    }
+
+    private static function binaryToString($binaryInput): ?string
+    {
+        $binaries = explode(' ', $binaryInput);
+
+        $string = null;
+        foreach ($binaries as $binary) {
+            $string .= pack('H*', dechex(bindec($binary)));
+        }
+
+        return $string;
     }
 }
