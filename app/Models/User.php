a66e165842cd8dd8a91151ba9d4016dc08bdd0d9
vtsybykov@gmail.com <Arct1cW138>
2024-04-06 22:50:02 +0800

    adding user_type fk

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,17 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property string $name
+ * @property string $second_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $phone
+ * @property string $login
+ * @property string $password
+ * @property int    $group_id
+ * @property int    $user_type_id
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -21,7 +32,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'type',
         'name',
         'second_name',
         'last_name',
@@ -29,6 +39,8 @@ class User extends Authenticatable
         'phone',
         'login',
         'password',
+        'group_id',
+        'user_type_id',
     ];
 
     /**
@@ -50,4 +62,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function type() : ?UserType {
+        if ($this->user_type_id === null) {
+            return null;
+        }
+        return UserType::find($this->user_type_id);
+
+    }
+
+    public function getTypeCode() : string{
+        return $this->type()?->code ?? 'guest';
+    }
 }
---
+++ database/migrations/2024_04_06_124032_change_user_type_to_fk.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\UserType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('type');
+            $table->foreignIdFor(UserType::class)->nullable()->constrained()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeignIdFor(UserType::class);
+            $table->dropColumn('user_type_id');
+            $table->string('type')->default('student');
+        });
+    }
+};
