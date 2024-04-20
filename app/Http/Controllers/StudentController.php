f424a293c9a6546a3b606eaa6e80d85eb4526581
vtsybykov@gmail.com <Arct1cW138>
2024-04-20 13:46:17 +0800

    loadExercises function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+
 class StudentController extends Controller
 {
 
-    public function getTasks() : array {
-        return [];
+    public function loadExercises() : Collection {
+        /** @var User $user */
+        $user = Auth::user();
+        return $user->getExercises() ?? Collection::empty();
     }
 
     public function uploadAnswer() : bool {
