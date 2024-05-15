758225cf021abb1b52db44cf864f1678aab890b8
vtsybykov@gmail.com <Arct1cW138>
2024-05-15 12:52:57 +0800

    TasksTableBlock.tsx improvements.

---
+++ resources/js/react/components/teacher/AttachmentsBlock.tsx
@@ -0,0 +1,17 @@
+import {Exercise} from "../../types/types";
+
+type Props= {
+    exercise: Exercise
+}
+
+const AttachmentsBlock = (props: Props) => {
+
+
+    return (
+        <div className="box theme-light">
+
+        </div>
+    )
+}
+
+export {AttachmentsBlock}
--- resources/js/react/components/teacher/TaskItem.tsx
+++ resources/js/react/components/teacher/TaskItem.tsx
@@ -1,4 +1,5 @@
 import {TaskWithTestStatusAndFile, UserWithFullName} from "../../types/types";
+import {dateForHumans} from "../../helpers/dateHepter";
 
 type Props = {
     task: TaskWithTestStatusAndFile
@@ -6,6 +7,14 @@ type Props = {
 }
 
 const TaskItem = (props: Props) => {
+
+
+    const viewFile = () => {
+        return <a href={'/labs-storage/' + props.task.file.generated_name + '.' + props.task.file.extension}
+           download={props.task.file.original_name + '.' + props.task.file.extension}>
+            {props.task.file.original_name + '.' + props.task.file.extension}
+        </a>
+    }
 
     const viewControls = () => {
         return <></>
@@ -16,8 +25,10 @@ const TaskItem = (props: Props) => {
             <td>{props.user !== null ? props.user.full_name : '—'}</td>
             <td>{props.task.test_status.label}</td>
             <td>{props.task.mark}</td>
-            <td>{props.task.last_uploaded_at}</td>
-            <td>{props.task.file.original_name}</td>
+            <td>{dateForHumans(props.task.last_uploaded_at, true)}</td>
+            <td>{viewFile()}</td>
+            <td>{props.task.comment}</td>
+            <td>{props.task.teacher_comment}</td>
             <td>{viewControls()}</td>
         </tr>
     )
