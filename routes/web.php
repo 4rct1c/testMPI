b0add9057cdecfc8baf6ae575911c32507c3ac52
vtsybykov@gmail.com <Arct1cW138>
2024-06-09 13:14:43 +0800

    grouping routes

--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,25 @@ Auth::routes();
 
 Route::get('/', function () {return redirect(\route('react'));})->name('default');
 
-//Route::get('/portal/', [MainController::class, 'reactWithoutRole'])->name('reactWithoutRole');
 Route::get('/portal/{path?}', [MainController::class, 'react'])
     ->where('path', '.*')->name('react');
 
-Route::get('/ssh/test/', [SSHController::class, 'test'])->name('testSSH');
 
+Route::group([
+    'prefix' => 'api'
+], function () {
+    Route::get('/courses/load/', [StudentController::class, 'loadCourses'])->name('loadCourses');
+    Route::get('/tasks/load/', [StudentController::class, 'loadTasks'])->name('loadTasks');
+    Route::post('/file/upload/', [StudentController::class, 'uploadFile'])->name('uploadFile');
 
-Route::get('/api/courses/load/', [StudentController::class, 'loadCourses'])->name('loadCourses');
-Route::get('/api/tasks/load/', [StudentController::class, 'loadTasks'])->name('loadTasks');
-Route::post('/api/file/upload/', [StudentController::class, 'uploadFile'])->name('uploadFile');
+    Route::get('/exercise/load/{id?}', [MainController::class, 'loadExercise'])->name('loadExercise');
+    Route::get('/user/load/', [MainController::class, 'loadUser'])->name('loadUser');
 
-Route::get('/api/exercise/load/{id?}', [MainController::class, 'loadExercise'])->name('loadExercise');
-Route::get('/api/user/load/', [MainController::class, 'loadUser'])->name('loadUser');
+    Route::get('/exercise/load-students/{id?}', [TeacherController::class, 'loadExerciseStudents'])->name('loadExerciseStudents');
+    Route::put('/exercise/update/', [TeacherController::class, 'updateExercise'])->name('updateExercise');
+    Route::get('/groups/load/', [TeacherController::class, 'loadGroups'])->name('loadGroups');
+    Route::post('/test/add/', [TeacherController::class, 'addTest'])->name('addTest');
+    Route::put('/test/update/', [TeacherController::class, 'updateTest'])->name('updateTest');
+});
 
-Route::get('/api/exercise/load-students/{id?}', [TeacherController::class, 'loadExerciseStudents'])->name('loadExerciseStudents');
-Route::put('/api/exercise/update/', [TeacherController::class, 'updateExercise'])->name('updateExercise');
-Route::get('/api/groups/load/', [TeacherController::class, 'loadGroups'])->name('loadGroups');
-Route::post('/api/test/add/', [TeacherController::class, 'addTest'])->name('addTest');
-Route::put('/api/test/update/', [TeacherController::class, 'updateTest'])->name('updateTest');
-
-//Route::get('/home', [HomeController::class, 'index'])->name('home');
+//Route::get('/ssh/test/', [SSHController::class, 'test'])->name('testSSH');
