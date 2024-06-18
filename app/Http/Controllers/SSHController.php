16667e96556651f316219f3b4b54769c94ea9216
vtsybykov@gmail.com <Arct1cW138>
2024-06-18 19:09:20 +0800

    adding admin check on job dispatching method

--- app/Http/Controllers/SSHController.php
+++ app/Http/Controllers/SSHController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\FilesSendDispatcherJob;
+use Illuminate\Support\Facades\Auth;
 
 class SSHController extends Controller
 {
 
     public function dispatchJob(){
+        if (!Auth::user()->isAdmin()){
+            abort(403);
+        }
         FilesSendDispatcherJob::dispatch(1);
     }
 }
